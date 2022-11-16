e90bab5cd9d22aad5da31857d3733c54c814302c
Cosmin Neaga <cosmin.neaga91@gmail.com>
2022-11-16 18:58:10 +0000

    added frontend

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-FILE='.env'
+FILE='./backend/.env'
 if [ -f $FILE ]; then
     echo "File $FILE exists."
 else
     echo "File $FILE does not exist, copying example.env."
-    cp example.env .env
+    cp ./backend/example.env ./backend/.env
 fi
 
 docker compose up -d --build
