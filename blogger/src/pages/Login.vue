b4d32fe134a7747edd581f144cc8471ad7f3a5ad
Cosmin Neaga <cosmin.neaga91@gmail.com>
2022-12-03 18:42:22 +0000

    updates

---
+++ blogger/src/pages/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <default-layout>
+        <v-card
+            variant="outlined"
+            width="500"
+            class="mx-auto mt-6 pa-4"
+        >
+
+            <template #title>
+                <h4 class="text-h4">Login to Blogger</h4>
+            </template>
+
+            <template #text>
+                <v-form>
+                    <v-text-field
+                        label="E-Mail"
+                        type="email"
+                        variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                    ></v-text-field>
+
+                    <v-btn
+                        color="blue"
+                    >Login</v-btn>
+                </v-form>
+            </template>
+
+        </v-card>
+    </default-layout>
+</template>
+
+<!--
+<script>
+export default {
+    data: () => ({
+        email: "user@gmail.com",
+        password: "password",
+    }),
+    methods: {
+        async login() {
+            const d = {
+                email: this.email,
+                password: this.password,
+            };
+            try {
+                const res = await $fetch(
+                    "http://localhost:8080/api/blogger/login",
+                    {
+                        method: "POST",
+                        body: d,
+                    }
+                );
+                console.log(res);
+                localStorage.setItem("blogger", JSON.stringify(res.data));
+                this.$router.push("/user/home");
+            } catch (e) {
+                console.log(e.response);
+                this.$toast.error(e.response._data.message);
+            }
+        },
+    },
+};
+</script>
+-->
--- blogger/src/router/index.ts
+++ blogger/src/router/index.ts
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from "../pages/Home.vue";
-import About from "../pages/About.vue";
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
+import Login from "@/pages/Login.vue";
+import Register from "@/pages/Register.vue";
+
 import NotFound from "@/pages/NotFound.vue";
 import Error from "@/pages/Error.vue";
 
@@ -20,6 +23,14 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: About,
+    },
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/register",
+      component: Register
     },
     {
       path: '/:pathMatch(.*)*',
