35a4789d93da677b6ee2e5a41db60a9c197c5afc
Cosmin Neaga <cosmin.neaga91@gmail.com>
2022-12-05 20:59:20 +0000

    updates

--- blogger/vite.config.ts
+++ blogger/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080/api/blogger',
+        target: 'http://localhost:8080/api/blogger',
         secure: false,
         ws: true,
         changeOrigin: true,
