7b9e05891b5e82431c18c834cb238dd674105979
Cosmin Neaga <cosmin.neaga91@gmail.com>
2023-01-31 22:53:51 +0000

    updates

--- blogger/src/main.ts
+++ blogger/src/main.ts
@@ -82,7 +82,8 @@ app
 app.component("default-layout", Default).component("user-layout", User);
 
 // config axios
-axios.defaults.baseURL = 'http://localhost:8080/api/blogger'
+// axios.defaults.baseURL = 'http://localhost:8080/api/blogger'
+axios.defaults.baseURL = 'http://80.208.225.17:8080/api/blogger'
 axios.interceptors.response.use(
   function (response: any) {
     if (response.data.status === 401) {
