57ed70169aff2f3d5c80fceaf4f51d7d5d9467ad
Cosmin Neaga <cosmin.neaga91@gmail.com>
2023-02-01 00:12:17 +0000

    update

--- blogger/src/pages/Logout.vue
+++ blogger/src/pages/Logout.vue
@@ -49,6 +49,15 @@ export default {
                 document.cookie = name + "=; expires=Thu, 01-Jan-1970 00:00:01 GMT;" + pathCurrent + ";";
             }
         },
+
+        deleteCookies() {
+            let allCookies = document.cookie.split(";");
+
+            // The "expire" attribute of every cookie is
+            // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+            for (var i = 0; i < allCookies.length; i++)
+                document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
+        },
     },
 };
 </script>
