ebe7439485d0089f223db89e3f4c5d887cb4f00e
Cosmin Neaga <57816741+cosminneaga@users.noreply.github.com>
2023-02-13 12:26:50 +0000

    formatting & comments

--- blogger/src/pages/User/Settings.vue
+++ blogger/src/pages/User/Settings.vue
@@ -10,8 +10,8 @@ const bloggerUrl = import.meta.env.VITE_BLOGGER_URL;
             color="deep-purple-accent-4"
             align-tabs="center"
         >
-            <v-tab :value="1">external use</v-tab>
-            <v-tab :value="2">profile</v-tab>
+            <v-tab :value="1">profile</v-tab>
+            <v-tab :value="2">external use</v-tab>
             <v-tab :value="3">create user</v-tab>
         </v-tabs>
 
@@ -20,36 +20,8 @@ const bloggerUrl = import.meta.env.VITE_BLOGGER_URL;
             style="max-width: 1000px; margin-top: 50px;"
             class="mx-auto"
         >
-
+            <!-- PROFILE -->
             <v-window-item :value="1">
-                <div v-highlight>
-                    <h2>How to use your external access token</h2>
-
-                    <br>
-                    <h3>dev url</h3>
-                    <br>
-                    <h4>Retrieve entire list of blogs</h4>
-                    <pre><code class="language-javascript">{{ bloggerUrl }}/external/blogs/{{user.access_token}}</code></pre>
-
-                    <br>
-                    <h3>Retrieve single blog by id</h3>
-                    <pre><code class="language-javascript">{{ bloggerUrl }}/external/blog/&lt;blogId&gt;/{{user.access_token}}</code></pre>
-
-                    <br>
-                    <h3>fetch</h3>
-                    <pre class="language-javascript"><code>fetch('{{ bloggerUrl }}/external/blogs/{{user.access_token}}')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-            </code></pre>
-                </div>
-            </v-window-item>
-
-            <v-window-item :value="2">
                 <ul v-highlight>
                     <li>Name: {{user.first_name}} {{user.last_name}}</li>
                     <li>Email: {{user.email}}</li>
@@ -59,11 +31,41 @@ const bloggerUrl = import.meta.env.VITE_BLOGGER_URL;
                     </li>
                 </ul>
             </v-window-item>
+            <!-- PROFILE -->
 
+            <!-- EXTERNAL USE -->
+            <v-window-item :value="2">
+                <div v-highlight>
+                    <h1>How to use your external access token</h1>
+
+                    <br>
+                    <h3>Retrieve entire list of blogs</h3>
+                    <pre><code class="language-javascript">{{ bloggerUrl }}/external/blogs/{{user.access_token}}</code></pre>
+
+                    <br>
+                    <h3>Retrieve single blog by id</h3>
+                    <pre><code class="language-javascript">{{ bloggerUrl }}/external/blog/&lt;blogId&gt;/{{user.access_token}}</code></pre>
+
+                    <br>
+                    <h3>Fetch</h3>
+                    <pre class="language-javascript"><code>fetch('{{ bloggerUrl }}/external/blogs/{{user.access_token}}')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });</code></pre>
+                </div>
+            </v-window-item>
+            <!-- EXTERNAL USE -->
+
+            <!-- CREATE USER -->
             <v-window-item :value="3">
                 <CreateUserForm />
             </v-window-item>
         </v-window>
+        <!-- CREATE USER -->
 
     </user-layout>
 </template>
