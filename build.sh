dc890fd1cc3ac717bea69b20af45341181ce814b
Cosmin Neaga <cosmin.neaga91@gmail.com>
2023-02-22 13:33:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-
 # ----------------------------- COPY BACKEND ENV ----------------------------- #
-ENV='./api/.env'
-if [ ! -f $ENV ]; then
-    echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./api/example.env $ENV
+FILE1='./api/.env'
+if [ ! -f $FILE1 ]; then
+    echo "File $FILE1 does not exist, copying 'example.env .'"
+    cp ./api/example.env $FILE1
 fi
 
 # ------------------------- COPY BLOGGER ENV ------------------------ #
-ENV='./blogger/.env'
-if [ ! -f $ENV ]; then
-    echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./blogger/example.env $ENV
+FILE2='./blogger/.env'
+if [ ! -f $FILE2 ]; then
+    echo "File $FILE2 does not exist, copying 'example.env .'"
+    cp ./blogger/example.env $FILE2
 fi
 
 # ----------------------------- COPY DATABASE ENV ---------------------------- #
-ENV='./db/.env'
-if [ ! -f $ENV ]; then
-    echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./db/example.env $ENV
+FILE3='./db/.env'
+if [ ! -f $FILE3 ]; then
+    echo "File $FILE3 does not exist, copying 'example.env .'"
+    cp ./db/example.env $FILE3
 fi
 
 # echo "DELETING ./blogger/node_modules... PLEASE WAIT"
