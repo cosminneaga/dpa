bc939af8725aa5fd491edf0ae297cf10a9aa9119
Cosmin Neaga <cosmin.neaga91@gmail.com>
2023-02-22 14:05:12 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 # ----------------------------- COPY BACKEND ENV ----------------------------- #
 ENV='./api/.env'
-if [ ! -f $ENV ]
-then
+if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
     cp ./api/example.env ./api/.env
 fi
 # ------------------------- COPY BLOGGER ENV ------------------------ #
 ENV='./blogger/.env'
-if [ ! -f $ENV ]
-then
+if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
     cp ./blogger/example.env ./blogger/.env
 fi
 # ------------------------- COPY DB ENV ------------------------ #
 ENV='./db/.env'
-if [ ! -f $ENV ]
-then
+if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
     cp ./db/example.env ./db/.env
 fi
