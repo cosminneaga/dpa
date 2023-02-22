37fc8411f41fa9659f948b4db1e5ccab69e2acc2
Cosmin Neaga <cosmin.neaga91@gmail.com>
2023-02-22 12:47:10 +0000

    changed commands in script

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 # ----------------------------- COPY DATABASE ENV ---------------------------- #
-ENV='./db/env/.env'
+ENVDB='./db/env/.env'
 if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./db/env/example.env $ENV
+    cp ./db/env/example.env $ENVDB
 fi
 
 # ----------------------------- COPY BACKEND ENV ----------------------------- #
-ENV='./api/.env'
+ENVAPI='./api/.env'
 if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./api/example.env $ENV
+    cp ./api/example.env $ENVAPI
 fi
 
 # ------------------------- COPY BLOGGER ENV ------------------------ #
-ENV='./blogger/.env'
+ENVBLOGGER='./blogger/.env'
 if [ ! -f $ENV ]; then
     echo "File $ENV does not exist, copying 'example.env .'"
-    cp ./blogger/example.env $ENV
+    cp ./blogger/example.env $ENVBLOGGER
 fi
 
 # echo "DELETING ./blogger/node_modules... PLEASE WAIT"
